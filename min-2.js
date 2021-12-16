7cac1837c24b7ea2724f41e5c62b68461a70a10b
VitaliiKotsyubinsky <magnoom2010@gmail.com>
2021-12-16 20:21:58 +0200

    min-2

---
+++ min-2.js
@@ -0,0 +1,24 @@
+let numberEnter = prompt('Введіть булдьяке число від 0-9')
+if (numberEnter == 0) {
+    alert(')')
+} else if (numberEnter == 1) {
+    alert('!')
+} else if (numberEnter == 2) {
+    alert('@')
+} else if (numberEnter == 3) {
+    alert('#')
+} else if (numberEnter == 4) {
+    alert('$')
+} else if (numberEnter == 5) {
+    alert('%')
+} else if (numberEnter == 6) {
+    alert('^')
+} else if (numberEnter == 7) {
+    alert('&')
+} else if (numberEnter == 8) {
+    alert('*')
+} else if (numberEnter == 9) {
+    alert('(')
+} else {
+    alert('не вірний форма')
+} 
